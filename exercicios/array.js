e9402ec01865420d6ab4bc88dbc568d8de201219
Carlos Daniel <73503556+senhordan@users.noreply.github.com>
2021-10-29 05:59:48 -0300

    Arrumando a pasta

--- exercicios/array.js
+++
@@ -1,9 +0,0 @@
-let variavel = [5, 1, 2, 9, 3]
-
-// for (let contador = 0;contador != variavel.length;contador++) {
-// 	console.log(variavel[contador])
-// }
-
-for (let x in variavel) {
-	console.log(variavel[x])
-}
--- teste.js
+++
@@ -1,3 +0,0 @@
-var myArray = new Array('1', '2', '3')
-
-console.log(myArray.unshift('4', '5'))
