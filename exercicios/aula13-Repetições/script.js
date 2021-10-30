7d19e888e758e2675fd29ffa71dbf3d58fa29d74
Carlos Daniel <73503556+senhordan@users.noreply.github.com>
2021-10-30 14:35:16 -0300

    Repetição

---
+++ exercicios/aula13-Repetições/script.js
@@ -0,0 +1,29 @@
+// WHILE
+
+	// Teste Logico No Inicio
+
+	/*var a = 0
+	while (a < 500) {
+		
+		a ++
+		console.log(`passo ${a}`)
+	}*/
+
+
+	// Teste Logico No Final
+
+	/*var a = 0
+	do{
+		a ++
+		console.log(`passo ${a}`)
+
+	} while (a < 500)*/
+
+// FOR
+
+for (var a = 1; a < 500; a ++) {
+	console.log(a)
+}
+
+
+
