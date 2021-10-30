4dc008d0888d23928a6d0516379b6a0fceb3fbc8
Carlos Daniel <73503556+senhordan@users.noreply.github.com>
2021-10-29 22:01:12 -0300

    Exercicios de Condicionais

---
+++ aula11-Condições/ex01.js
@@ -0,0 +1,12 @@
+// console.log("Funcionou!")
+var velocidade = 60.5
+
+console.log(`a velocidade do seu carro é ${velocidade}`)
+if (velocidade > 60) {
+	console.log(`Você ultrapassou a velocidade permitida, MULTADO!!`)
+
+}
+console.log(`Dirija com cuidado!`)
+
+
+
